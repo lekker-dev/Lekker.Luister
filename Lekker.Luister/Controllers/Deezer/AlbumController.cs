e4ffb3dc9e013693af964dcea8ee22dc8a720fc0
Gerhard Mostert <gerhard.mostert83@gmail.com>
2020-10-13 13:40:45 +0200

    RAG - implemented the template for the Deezer 'GetAlbum' method on the album controller - added the album and track response models

--- Lekker.Luister/Controllers/Deezer/AlbumController.cs
+++ Lekker.Luister/Controllers/Deezer/AlbumController.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using Lekker.Luister.Models.Response;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lekker.Luister.Controllers.Deezer
 {
-    public class AlbumController
+    [ApiController]
+    [Route("deezer/album/")]
+    public class AlbumController : Controller
     {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DeezerAlbumResponse>> GetAlbum(int id)
+        {
+            var album = new DeezerAlbumResponse()
+            {
+                Title = "Fuz fuz",
+                ArtistName = "The Knacks",
+                CoverUrl = new Uri("https://e-cdns-images.dzcdn.net/images/cover/8e119aaa57aafc3266bc427acb942a9b/250x250-000000-80-0-0.jpg"),
+                Tracks = new List<DeezerTrackResponse>
+                {
+                    new DeezerTrackResponse
+                    {
+                        Number = 1,
+                        Name = "The floox cruze",
+                        Duration = TimeSpan.FromMinutes(3.1)
+                        
+                    }
+                },
+                Link = new Uri("https://www.deezer.com/album/100000")
+
+            };
+
+            return Ok(album);
+        }
     }
 }
---
+++ Lekker.Luister/Models/Response/DeezerAlbumResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lekker.Luister.Models.Response
+{
+    public class DeezerAlbumResponse
+    {
+        /// <summary>
+        /// The title of the album.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The name of the album artist
+        /// </summary>
+        public string ArtistName { get; set; }
+
+        /// <summary>
+        /// The URL to the image of the album art.
+        /// </summary>
+        public Uri CoverUrl { get; set; }
+
+        /// <summary>
+        /// The track listing.
+        /// </summary>
+        public List<DeezerTrackResponse> Tracks { get; set; }
+
+        /// <summary>
+        /// The URL to the album on the Deezer service.
+        /// </summary>
+        public Uri Link { get; set; }
+    }
+}
